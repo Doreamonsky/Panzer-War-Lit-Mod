d58e4b84e80eedadeb58f6dcc3d06289280539d6
Wang Shijiang <403036847@qq.com>
2019-03-02 18:24:02 +0800

    Fix material-fix

---
+++ OfficialAddOns/MaterialFix/MaterialFix/MaterialFix.cs
@@ -0,0 +1,17 @@
+﻿using ShanghaiWindy.Core;
+using System.Linq;
+using UnityEngine;
+using UnityMod;
+
+namespace MaterialFix
+{
+    public class MaterialFix : IVehicleAddOn
+    {
+        public void OnVehicleLoaded(int instanceID)
+        {
+            var playerVehicle = GameObject.FindObjectsOfType<TankInitSystem>().ToList().Find(x => x.gameObject.GetInstanceID() == instanceID);
+
+            AssetShaderManager.ReplaceShader(playerVehicle.gameObject, true);
+        }
+    }
+}
