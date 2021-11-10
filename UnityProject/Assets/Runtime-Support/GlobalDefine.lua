8544395d667eef3353c15f5d052d14edddd19943
超级哆啦酱 <403036847@qq.com>
2021-11-10 23:18:19 +0800

    fix bugs

--- UnityProject/Assets/Runtime-Support/GlobalDefine.lua
+++ UnityProject/Assets/Runtime-Support/GlobalDefine.lua
@@ -12,6 +12,9 @@
 ---@field RequestScene fun(sceneName:string,onLoaded:System.Action):void 异步加载场景
 ---@field CreateDIYVehicle fun(userDefined:DIYUserDefined,onLoaded:System.Action<GameObject, VehicleTextData>):void 异步加载DIY载具
 ---@field GetGUID fun():string 创建 GUID
+---@field SetRuleAsMain fun(userDefined:DIYUserDefined,mainRuleGuid:string) 设置 Rule 为 Main
+---@field ExportShareCode fun(userDefined:DIYUserDefined,onComplete:System.Action<string>)
+---@field ImportShareCode fun(shareId:string,onComplete:System.Action<DIYUserDefined>)
 ---@field OnLuaExitModeReq UnityEvent
 CSharpAPI = CS.ShanghaiWindy.Core.LuaCallCSharpHelper
 
@@ -50,10 +53,11 @@ Core = CS.ShanghaiWindy.Core
 MouseLockModule = Core.MouseLockModule
 
 --- @class DIYUserDefined
---- @field rules List<Rule>
+--- @field rules List<DIYRule>
 --- @field overrideRank number
 --- @field perferredRank number
 --- @field definedName string
+--- @field GetDeepCopied fun():DIYUserDefined 深拷贝自己
 DIYUserDefined = CS.ShanghaiWindy.Core.Data.DIYUserDefined
 
 --- @class DIYRule
@@ -65,11 +69,20 @@ DIYUserDefined = CS.ShanghaiWindy.Core.Data.DIYUserDefined
 --- @field scaleSize SerializeVector3
 --- @field deltaPos SerializeVector3
 --- @field localEulerAngles SerializeVector3
+--- @field customPropertiesJson string
 DIYRule = CS.ShanghaiWindy.Core.Data.DIYUserDefined.Rule
 
 DIYDataEnum = CS.ShanghaiWindy.Core.Data.DIYDataEnum
 SerializeVector3 = CS.ShanghaiWindy.Core.Data.SerializeVector3
+uGUI_Localization = CS.ShanghaiWindy.Core.uGUI_Localization
 
+OutlineHelper = CS.OutlineHelper
+HitBox = CS.ShanghaiWindy.Core.HitBox
+InternalModule = CS.ShanghaiWindy.Core.InternalModule
+
+--- @class uGUI_Localsize
+--- @field GetContent fun(_Key:string):void
+uGUI_Localsize = CS.ShanghaiWindy.Core.uGUI_Localsize
 -- UnityEngine
 --- @class GameObject
 --- @field Instantiate fun() 实例化
@@ -82,8 +95,23 @@ Color = CS.UnityEngine.Color
 
 --- @class JsonUtility
 --- @field ToJson fun(object:obj) Generate a JSON representation of the public fields of an object.
+--- @field FromJsonOverwrite fun(json:string,overrideTarget:object)
 JsonUtility = CS.UnityEngine.JsonUtility
 
 -- UGUI
 Text = CS.UnityEngine.UI.Text
 Image = CS.UnityEngine.UI.Image
+Canvas = CS.UnityEngine.Canvas
+
+-- Web
+WWWForm = CS.UnityEngine.WWWForm
+UnityWebRequest = CS.UnityEngine.Networking.UnityWebRequest
+
+-- Common
+--- @class Application
+--- @field lowMemory Action
+Application = CS.UnityEngine.Application
+
+Time = CS.UnityEngine.Time
+
+MeshRenderer = CS.UnityEngine.MeshRenderer
