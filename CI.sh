cb8c341cce38852239209f5816c982bf9ee9bade
WangShijiang <403036847@qq.com>
2022-07-12 23:21:06 +0800

    Auto Submit

--- CI.sh
+++ CI.sh
@@ -1,5 +1,5 @@
 time=$(date "+%Y%m%d%H%M%S")
-./zip.exe -r $time-UnityProject.zip UnityProject
+./zip.exe -r $time-UnityProject.zip UnityProject/Assets
 git add .
 git commit -m "Auto Submit"
 git push
