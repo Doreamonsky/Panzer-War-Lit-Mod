f3f7a2a25f8c2b0118708ae403387ce731ade2ab
WangShijiang <403036847@qq.com>
2022-07-12 23:28:34 +0800

    Auto Submit

--- CI.sh
+++ CI.sh
@@ -1,5 +1,20 @@
 time=$(date "+%Y%m%d%H%M%S")
-./zip.exe -r $time-UnityProject.zip UnityProject/Assets
+# 创建缓存工程
+releaseName=UnityProject-Release
+output=$time-UnityProject.zip
+
+mkdir $releaseName
+
+cp -r  UnityProject/Assets $releaseName/Assets
+cp -r  UnityProject/ProjectSettings $releaseName/ProjectSettings
+cp -r  UnityProject/Packages $releaseName/Packages
+cp -r  UnityProject/BuildPiplineCI $releaseName/BuildPiplineCI
+
+./zip.exe -r $output $releaseName
+
+rm -r -f $releaseName
+mv $output archive/$output
+
 git add .
 git commit -m "Auto Submit"
 git push
