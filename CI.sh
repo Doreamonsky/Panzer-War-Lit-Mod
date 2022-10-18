ccff645b740696db331e66667696999cf73b209b
Doreamonsky <403036847@qq.com>
2022-10-18 22:07:09 +0800

    增加 VFX 标注

--- CI.sh
+++ CI.sh
@@ -1,4 +1,17 @@
+#!/bin/bash
+
 time=$(date "+%Y%m%d%H%M%S")
+
+echo "Start to build the project from CI tool."
+echo "Input the modified comment:"
+read comment
+
+# 提交 Commit
+git add .
+git commit -m $comment
+git push
+git push github
+
 # 创建缓存工程
 releaseName=UnityProject-Release
 output=$time-UnityProject.zip
@@ -10,13 +23,9 @@ cp -r  UnityProject/ProjectSettings $releaseName/ProjectSettings
 cp -r  UnityProject/Packages $releaseName/Packages
 cp -r  UnityProject/BuildPiplineCI $releaseName/BuildPiplineCI
 cp -r  UnityProject/RuntimeRes $releaseName/RuntimeRes
+cp -r  更新工程必看.txt $releaseName/更新工程必看.txt
 
 ./zip.exe -r $output $releaseName
 
 rm -r -f $releaseName
 mv $output archive/$output
-
-git add .
-git commit -m "Auto Submit"
-git push
-git push github
