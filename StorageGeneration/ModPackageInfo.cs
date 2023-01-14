39e8f65391698c6ee1ae0ef61170d8fa9cd7fb76
Doreamonsky <403036847@qq.com>
2023-01-14 21:52:33 +0800

    update proj

---
+++ StorageGeneration/ModPackageInfo.cs
@@ -0,0 +1,35 @@
+﻿namespace ShanghaiWindy.Core
+{
+    [System.Serializable]
+    public struct ModFileManifiest
+    {
+        public string n;
+        public string h;
+        public string[] d;
+    }
+
+    [System.Serializable]
+    public struct ModPackageDependencyData
+    {
+        public string packageName;
+        public string packageGuid;
+        public int packageMiniVersion;
+    }
+
+    [System.Serializable]
+    public class ModPackageInfo
+    {
+        public string modName;
+        public string description;
+        public string author;
+        public string supportURL;
+        public int modVersion;
+        public string buildTarget;
+        public string buildEngineVersion;
+        public string uuid;
+
+        public string guid;
+        public ModFileManifiest[] fileManifests = new ModFileManifiest[] { };
+        public ModPackageDependencyData[] dependencies = new ModPackageDependencyData[] { };
+    }
+}
