9e370bddc190fe637da019597b42757996e0b1d5
Doreamonsky <403036847@qq.com>
2023-03-25 02:17:24 +0800

    add callback for propvalue

--- MiddleLibLayer/LuaComponents/Tutorial/GetVehicleHP.lua
+++ MiddleLibLayer/LuaComponents/Tutorial/GetVehicleHP.lua
@@ -2,11 +2,20 @@ Behavior()
 Property = {}
 local GetVehicleHP = class("GetVehicleHP")
 
+function GetVehicleHP:OnStarted()
+    self.OnTotalDamageChanged = function(oldVal, newVal)
+        print(tostring(oldVal) .. " -> " .. tostring(newVal))
+    end
+
+    self.vehicle.basePlayerState.totalDamage:OnValueChanged("+", self.OnTotalDamageChanged)
+end
+
 function GetVehicleHP:OnUpdated()
     local hp = self.vehicle.basePlayerState.hp:Get()
-    print(hp)
+    -- print(hp)
 
-    -- self.vehicle.basePlayerState.hp:Set(1500)
+    local totalDamage = self.vehicle.basePlayerState.totalDamage:Get()
+    -- print(totalDamage)
 end
 
 return GetVehicleHP
